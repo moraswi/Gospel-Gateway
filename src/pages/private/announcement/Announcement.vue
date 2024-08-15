299f11fdfc10b642db8fd184808a6c819bb55cda
moraswi <moraswip97@gmail.com>
2024-08-15 23:21:32 +0200

    announcements

--- src/pages/private/announcement/Announcement.vue
+++ src/pages/private/announcement/Announcement.vue
@@ -3,7 +3,12 @@
     <TheHeader title="Service program"> </TheHeader>
 
     <v-row class="mt-3">
-      <v-col cols="12" md="6">
+      <v-col
+        v-for="(branch, index) in getChurchPlanByBranchId"
+        :key="index"
+        cols="12"
+        md="6"
+      >
         <v-card class="custom-card px-7 py-6 mt-2" width="100%">
           <v-layout>
             <!-- row align-center -->
@@ -12,57 +17,11 @@
               </div>
               <v-spacer></v-spacer> -->
             <div class="text-left">
-              <h4 class="font-p">Lorem Ipsum</h4>
+              <h4 class="font-p">{{ branch.title }}</h4>
               <p class="font-p4 mt-1">
-                is simply dummy text of the printing and typesetting industry.
-                Lorem Ipsum has been the industry's standard dummy text ever
-                since the 1500s, when an unknown printer took a galley of type
-                and scrambled it to make a type specimen book. It has survived
-                not only five centuries
+                {{ branch.description }}
               </p>
-            </div>
-          </v-layout>
-        </v-card>
-      </v-col>
-      <v-col cols="12" md="6">
-        <v-card class="custom-card px-7 py-6 mt-2" width="100%">
-          <v-layout>
-            <!-- row align-center -->
-            <!-- <div>
-                <h1 class="font-h4 mt-n1">member name</h1>
-              </div>
-              <v-spacer></v-spacer> -->
-            <div class="text-left">
-              <h4 class="font-p">Lorem Ipsum</h4>
-              <p class="font-p4 mt-1">
-                is simply dummy text of the printing and typesetting industry.
-                Lorem Ipsum has been the industry's standard dummy text ever
-                since the 1500s, when an unknown printer took a galley of type
-                and scrambled it to make a type specimen book. It has survived
-                not only five centuries
-              </p>
-            </div>
-          </v-layout>
-        </v-card>
-      </v-col>
-      <v-col cols="12" md="6">
-        <v-card class="custom-card px-7 py-6 mt-2" width="100%">
-          <v-layout>
-            <!-- row align-center -->
-            <!-- <div>
-                <h1 class="font-h4 mt-n1">member name</h1>
-              </div>
-              <v-spacer></v-spacer> -->
-            <div class="text-left">
-              <h4 class="font-p">Lorem Ipsum</h4>
-              <p class="font-p4 mt-1">
-                is simply dummy text of the printing and typesetting industry.
-                Lorem Ipsum has been the industry's standard dummy text ever
-                since the 1500s, when an unknown printer took a galley of type
-                and scrambled it to make a type specimen book. It has survived
-                not only five centuries
-              </p>
-              <h4 class="font-p">moledi@gmail.com</h4>
+              <h4 class="font-p">{{ branch.contact }}</h4>
             </div>
           </v-layout>
         </v-card>
@@ -90,5 +49,17 @@ export default {
   data: () => ({
     overlay: false,
   }),
+
+  async created() {
+    await Promise.all([
+      this.$store.dispatch("announcement/getAnnouncementByChurchIdReq", 1),
+    ]);
+  },
+
+  computed: {
+    getChurchPlanByBranchId() {
+      return this.$store.getters["announcement/getAnnouncementByChurchId"];
+    },
+  },
 };
 </script>
